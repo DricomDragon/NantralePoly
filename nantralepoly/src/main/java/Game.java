e433bd9c59f0e17eab3b82f418fcefbcb4fa0255
Syned <synedday@gmail.com>
2019-11-14 11:31:20 +0100

    Initialize PlateauDeJeu

--- nantralepoly/src/main/java/Game.java
+++ nantralepoly/src/main/java/Game.java
@@ -1,8 +1,13 @@
+import plateau.PlateauDeJeu;
 
 class Game {
 
     public static void main(String[] args) {
         System.out.println("NantralePoly demarre.");
+
+        PlateauDeJeu plateauDeJeu = new PlateauDeJeu();
+        plateauDeJeu.init();
+
         System.out.println("Fin du programme.");
     }
 }
