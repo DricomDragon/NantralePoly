4e5bf109f00fae1db2d9c1931eecc6f467cfe420
Syned <synedday@gmail.com>
2019-11-14 11:03:13 +0100

    Add Joueur

--- nantralepoly/src/main/java/joueur/Joueur.java
+++ nantralepoly/src/main/java/joueur/Joueur.java
@@ -1,9 +1,47 @@
 package joueur;
 
+import emplacement.Achetable;
+import emplacement.Case;
+
 public class Joueur {
 
+    public String nom;
+    public int fortune;
+    public Case position;
 
     public String getNom() {
-        return "";
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getFortune() {
+        return fortune;
+    }
+
+    public void setFortune(int fortune) {
+        this.fortune = fortune;
+    }
+
+    public Case getPosition() {
+        return position;
+    }
+
+    public void setPosition(Case position) {
+        this.position = position;
+    }
+
+    /**
+     * j1.paiement(j2) le joueur j2 tombe sur une case dont j1 est le
+     * propriétaire l'argent va donc de j2 vers j1
+     */
+    public void paiement(Joueur j) {
+        if (getPosition() instanceof Achetable) {
+            int somme = Math.min(((Achetable) getPosition()).loyer(), j.getFortune()); // pour gérer le cas où le joueur n'a pas assez d'argent pour payer le loyer
+            this.setFortune(this.getFortune() + somme);
+            j.setFortune(j.getFortune() - somme);
+        }
     }
 }
